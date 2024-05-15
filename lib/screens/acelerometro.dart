e4ec2b7651354491c3c101d1c40415c0db053106
BrayanJavierB <brayanj26becerra703@gmail.com>
2024-05-14 22:59:13 -0500

    uso del hardware

--- lib/screens/acelerometro.dart
+++ lib/screens/acelerometro.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
+import 'package:camera/camera.dart';
 
 class SensorScreen extends StatefulWidget {
   @override
@@ -8,19 +9,54 @@ class SensorScreen extends StatefulWidget {
 
 class _SensorScreenState extends State<SensorScreen> {
   AccelerometerEvent? _accelerometerValues;
+  late CameraController _cameraController;
+  late bool _isFlashOn;
 
   @override
   void initState() {
     super.initState();
-    _initializeSensor(); // Llama al método para iniciar la suscripción al evento del acelerómetro.
+    _initializeSensor();
+    _initializeCamera();
+    _isFlashOn = false;
   }
 
   void _initializeSensor() {
     accelerometerEvents.listen((AccelerometerEvent event) {
       setState(() {
-        _accelerometerValues = event; // Actualiza los valores del acelerómetro en tiempo real.
+        _accelerometerValues = event;
+        if (_accelerometerValues!.x > 8 ||
+            _accelerometerValues!.y > 8 ||
+            _accelerometerValues!.z > 8) {
+          _toggleFlashlight();
+        }
       });
     });
+  }
+
+  void _initializeCamera() {
+    _cameraController = CameraController(
+      CameraDescription(
+        name: '0',
+        lensDirection: CameraLensDirection.back,
+        sensorOrientation:
+            0, // Puedes ajustar este valor según la orientación del sensor
+      ),
+      ResolutionPreset.low,
+    );
+    _cameraController.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+    });
+  }
+
+  void _toggleFlashlight() {
+    if (_isFlashOn) {
+      _cameraController.setFlashMode(FlashMode.off);
+    } else {
+      _cameraController.setFlashMode(FlashMode.torch);
+    }
+    _isFlashOn = !_isFlashOn;
   }
 
   @override
@@ -51,5 +87,11 @@ class _SensorScreenState extends State<SensorScreen> {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
   }
 }
